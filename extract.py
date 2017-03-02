8b9c436c9187bb47750675da3c30af3dcc09b06c
raagavendran <raagavendran@gmail.com>
2017-03-02 10:34:44 +0530

    first push

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
 import re
 
+file = open('hotstar.txt','rb').read()#open file
 
-file = open('hotstar.txt','rb').read()
-
+#regular expression to extract from url
 ext = re.compile(r'.*\://www.hotstar.com/.*/.*/.*/.*/(.*)',re.IGNORECASE)
 final = re.findall(ext,file)
 for a in final:
